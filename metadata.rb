297caffd1c401242878370175414bb7cba22d3e6
jtimberman <joshua@opscode.com>
2013-04-19 09:32:58 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures Windows Installer XML toolset (WiX)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 supports         "windows"
 
 depends          "windows", ">= 1.2.2"
