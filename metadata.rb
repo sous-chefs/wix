a4594b2d965a47af270d39be803a947d1f03a58d
Tim Smith <tim@cozy.co>
2015-08-21 11:33:40 -0700

    Update copyright / add new metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
-name             "wix"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Installs/Configures Windows Installer XML toolset (WiX)"
+name 'wix'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs/Configures Windows Installer XML toolset (WiX)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
-supports         "windows"
+version '1.1.1'
+supports 'windows'
 
-depends          "windows", ">= 1.2.2"
+depends 'windows', '>= 1.2.2'
+
+source_url 'https://github.com/opscode-cookbooks/wix' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/wix/issues' if respond_to?(:source_url)
